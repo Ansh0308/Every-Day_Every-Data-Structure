b8c678db7430fbcc3d1bd418a15071cb8f162539
Ansh0308 <anshraythatha123@gmail.com>
2025-07-22 21:04:39 +0530

    Added Bubble sort in algorithems array

---
+++ Algorithms/Array/BubbleSort.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void val_array(vector<int> &array){
+    for(int i = 0; i < array.size(); i++){
+        cin >> array[i];
+    }
+    return;
+}
+void print_array(vector<int> &nums){
+    for(int ele : nums){
+        cout<<ele<<" ";
+    }
+    cout<<endl;
+}
+void BubbleSort(vector<int> &nums){
+    int n=nums.size();
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1-i;j++){
+            if(nums[j]>nums[j+1]){
+                swap(nums[j],nums[j+1]);
+            }
+        }
+    }
+    return ;
+}
+
+
+int main() {
+    cout << "Plz enter the n:" << endl;
+    int n;
+    cin >> n;
+    vector<int> array(n);
+    
+
+    cout << "Plz enter the array:" << endl;
+    val_array(array);
+    BubbleSort(array);
+    cout<<"The Sorted array  is "<<endl;
+    
+    print_array(array);
+
+
+
+    return 0;
+}
