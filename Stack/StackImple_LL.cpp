4c6c62f4b86e57db133e1ae4e28fa32a6c29e9e2
Ansh0308 <anshraythatha123@gmail.com>
2026-01-22 20:55:47 +0530

    Added the stack implementation using LinkedList

---
+++ Stack/StackImple_LL.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// -------------------------------
+// 🔹 Shortcuts & Typedefs
+// -------------------------------
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+
+// -------------------------------
+// 🔹 Macros
+// -------------------------------
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+#define endl '\n'
+#define sz(x) (int)(x).size()
+
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define for1(i, n) for (int i = 1; i <= (n); i++)
+#define forr(i, n) for (int i = (n)-1; i >= 0; i--)
+
+// -------------------------------
+// 🔹 Input / Output Helpers
+// -------------------------------
+vi input_vi(int n = -1) {
+    if (n == -1) cin >> n;
+    vi v(n);
+    forn(i, n) cin >> v[i];
+    return v;
+}
+
+vll input_vll(int n = -1) {
+    if (n == -1) cin >> n;
+    vll v(n);
+    forn(i, n) cin >> v[i];
+    return v;
+}
+
+template<typename T>
+void print_vec(const vector<T>& v, string sep = " ") {
+    for (auto &x : v) cout << x << sep;
+    cout << endl;
+}
+
+// -------------------------------
+// 🔹 Debug Utility
+// -------------------------------
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << x << endl
+#else
+#define dbg(x)
+#endif
+
+// -------------------------------
+// 🔹 Constants
+// -------------------------------
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+const double EPS = 1e-9;
+
+// -------------------------------
+// 🔹 Main Logic Example
+// -------------------------------
+class Stack{
+    list<int> ll;
+    public:
+    void push(int val){//O(1)
+        ll.push_front(val);
+    }
+    void pop(){
+        if(ll.size()==0){
+            cout<<"Stack is empty"<<endl;
+            return;
+        
+        }
+        ll.pop_front();
+    }
+    int top(){
+        if(ll.size()==0){
+            cout<<"Stack is empty"<<endl;
+            return -1;
+        }
+        return ll.front();
+    }
+    bool empty(){
+        return ll.size()==0;
+    }
+    
+
+
+};
+void solve() {
+    Stack st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    while(!st.empty()){
+        cout<<st.top()<<endl;
+        st.pop();
+    }
+    
+
+    
+   
+    
+    
+
+}
+
+// -------------------------------
+// 🔹 Entry Point
+// -------------------------------
+int main() {
+    solve();
+    return 0;
+}
