c9f71f3b255ad5d86040f61b319c591bfde7487f
Ansh0308 <anshraythatha123@gmail.com>
2026-01-23 12:33:21 +0530

    Added vaild Parenthese in Stack

---
+++ Stack/VaildParentheses.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// -------------------------------
+// 🔹 Shortcuts & Typedefs
+// -------------------------------
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+
+// -------------------------------
+// 🔹 Macros
+// -------------------------------
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+#define endl '\n'
+#define sz(x) (int)(x).size()
+
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define for1(i, n) for (int i = 1; i <= (n); i++)
+#define forr(i, n) for (int i = (n)-1; i >= 0; i--)
+
+// -------------------------------
+// 🔹 Input / Output Helpers
+// -------------------------------
+vi input_vi(int n = -1) {
+    if (n == -1) cin >> n;
+    vi v(n);
+    forn(i, n) cin >> v[i];
+    return v;
+}
+
+vll input_vll(int n = -1) {
+    if (n == -1) cin >> n;
+    vll v(n);
+    forn(i, n) cin >> v[i];
+    return v;
+}
+
+template<typename T>
+void print_vec(const vector<T>& v, string sep = " ") {
+    for (auto &x : v) cout << x << sep;
+    cout << endl;
+}
+
+// -------------------------------
+// 🔹 Debug Utility
+// -------------------------------
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << x << endl
+#else
+#define dbg(x)
+#endif
+
+// -------------------------------
+// 🔹 Constants
+// -------------------------------
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+const double EPS = 1e-9;
+
+// -------------------------------
+// 🔹 Main Logic Example
+// -------------------------------
+bool vaildParentheses(string &s){
+    stack<char> st;
+    for(char c:s){
+        if(c=='('||c=='['||c=='{'){
+            st.push(c);
+        }
+        else{
+            if(st.empty())return false;
+            else if(c==')'&&st.top()=='('){
+                st.pop();
+            }
+            else if(c==']'&&st.top()=='['){
+                st.pop();
+            }
+            else if(c=='}'&&st.top()=='{'){
+                st.pop();
+            }
+            else{
+                return false;
+            }
+        }
+    }
+    return st.empty();
+}
+void solve() {
+    string s;
+    s="{[()]}";
+    if(vaildParentheses(s)){
+        cout<<"Valid Parentheses"<<endl;
+    }
+    else{
+        cout<<"Invalid Parentheses"<<endl;
+    }
+    
+    
+
+}
+
+// -------------------------------
+// 🔹 Entry Point
+// -------------------------------
+int main() {
+    solve();
+    return 0;
+}
