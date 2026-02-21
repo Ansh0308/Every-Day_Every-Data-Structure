91b6e9cd8ed3e4ac3a2d26b6d5035ed6baf33d6b
Ansh0308 <anshraythatha123@gmail.com>
2026-02-21 21:23:12 +0530

    Stack Implementation using two Queues in Queue

---
+++ Queue/StackImplementationUsingQueue.cpp
@@ -0,0 +1,214 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// -------------------------------
+// 🔹 Shortcuts & Typedefs
+// -------------------------------
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+
+// -------------------------------
+// 🔹 Macros
+// -------------------------------
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+#define endl '\n'
+#define sz(x) (int)(x).size()
+
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define for1(i, n) for (int i = 1; i <= (n); i++)
+#define forr(i, n) for (int i = (n)-1; i >= 0; i--)
+
+// -------------------------------
+// 🔹 Input / Output Helpers
+// -------------------------------
+vi input_vi(int n = -1) {
+    if (n == -1) cin >> n;
+    vi v(n);
+    forn(i, n) cin >> v[i];
+    return v;
+}
+
+vll input_vll(int n = -1) {
+    if (n == -1) cin >> n;
+    vll v(n);
+    forn(i, n) cin >> v[i];
+    return v;
+}
+
+template<typename T>
+void print_vec(const vector<T>& v, string sep = " ") {
+    for (auto &x : v) cout << x << sep;
+    cout << endl;
+}
+
+// -------------------------------
+// 🔹 Debug Utility
+// -------------------------------
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << x << endl
+#else
+#define dbg(x)
+#endif
+
+// -------------------------------
+// 🔹 Constants
+// -------------------------------
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+const double EPS = 1e-9;
+
+// -------------------------------
+// 🔹 Main Logic Example
+// -------------------------------
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int val){
+        data = val;
+        next = NULL;
+}
+};
+class Queue{
+    public:
+    Node* head;
+    Node* tail;
+    Queue(){
+        head = NULL;
+        tail = NULL;
+    }
+    void push(int val){
+        if(!head){
+            head=new Node(val);
+            tail=head;
+            return;
+        }
+        tail->next=new Node(val);
+        tail=tail->next;
+
+    }
+    void pop(){
+        if(!head)return;
+        Node* temp=head;
+        head=head->next;
+        delete temp;
+
+    }
+    int front(){
+        if(!head)return -1;
+        return head->data;
+    }
+
+    bool empty(){
+        return head == NULL;
+    }
+};
+class CircularQueue{
+    public:
+    int *arr;
+    int front=0;
+    int rear=-1;
+    int size,cap;
+    CircularQueue(int cap){
+        size=0;
+        this->cap=cap;
+        arr=new int[cap];
+    } 
+    void push(int val){
+        if(size==cap){
+            cout<<"Queue is full"<<endl;
+            return;
+        }
+        rear=(rear+1)%cap;
+        arr[rear]=val;
+        size++;
+    }
+    void pop(){
+        if(size==0){
+            cout<<"Queue is empty"<<endl;
+            return;
+        }
+        front=(front+1)%cap;
+        size--;
+    }
+    int frontEle(){
+        if(size==0){
+            cout<<"Queue is empty"<<endl;
+            return -1;
+        }
+        return arr[front];
+    }
+    bool empty(){
+        return size==0;
+    }
+};
+class StackUsingQueue{
+    public:
+    queue<int> q1,q2;
+    void push(int x){
+        if(q1.empty()){
+            q1.push(x);
+        }
+        else{
+            while(!q1.empty()){
+                q2.push(q1.front());
+                q1.pop();
+            }
+            q1.push(x);
+            while(!q2.empty()){
+                q1.push(q2.front());
+                q2.pop();
+            }
+        }
+    }
+    int pop(){
+        if(q1.empty()){
+            cout<<"Stack is empty"<<endl;
+            return -1;
+        }
+        int top=q1.front();
+        q1.pop();
+        return top;
+    }
+    int top(){
+        if(q1.empty()){
+            cout<<"Stack is empty"<<endl;
+            return -1;
+        }
+        return q1.front();
+    }
+    bool empty(){
+        return q1.empty();
+    }
+};
+
+void solve() {
+    StackUsingQueue s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    while(!s.empty()){
+        cout<<s.top()<<endl;
+        s.pop();
+    }
+    
+}
+
+// -------------------------------
+// 🔹 Entry Point
+// -------------------------------
+int main() {
+    solve();
+    return 0;
+}
